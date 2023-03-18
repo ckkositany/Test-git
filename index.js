09dd8fab4e5715bc77cc11a7a9b64122353f60f9
ckkositany <kositanyck@gmail.com>
2023-03-18 14:09:09 +0300

    Adding git notes

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log("Hello world")
+//introduction to git
+/*basic git commands
+1. $ git init: to initialize a local git repo
+2. $ git add<file>: to add files to index
+3. $ git status : to check statusof working tree
+4. $ git commit : to commit changes in index
+5. $ git push : push to your remote repo eg gitHub
+6. $ git pull: pull latest from remote repo
+7. $ git clone : clone repo into a new dir
+
+*/
